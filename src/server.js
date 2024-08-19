bfb36b238ac9dc016d50ef63ec724ccff6e7b6c4
razisyahputro <razisyahputro@gmail.com>
2024-08-19 15:12:05 +0700

    remove unecessary file

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
-const notesPlugin = require('./notesPlugin');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 3000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
@@ -13,12 +12,7 @@ const init = async () => {
     },
   });
 
-  // server.route(routes);
-
-  await server.register({
-    plugin: notesPlugin,
-    options: { notes: [] }
-  })
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
